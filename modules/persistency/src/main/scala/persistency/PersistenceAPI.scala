e41ca507b829f427aeaa777a125492e6f22cacf6
Julian Böhne <julian.boehne@web.de>
2024-05-07 14:36:55 +0200

    add APIController

--- modules/persistency/src/main/scala/persistency/PersistenceAPI.scala
+++ modules/persistency/src/main/scala/persistency/PersistenceAPI.scala
@@ -8,6 +8,8 @@ import akka.http.scaladsl.server.Directives.*
 import com.google.inject.{Guice, Injector}
 import persistency.IO.{FileIOJson, FileIOXml}
 import play.api.libs.json.{Json, Writes}
+import akka.http.scaladsl.unmarshalling.{FromStringUnmarshaller, Unmarshaller}
+import scala.util.Try
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
@@ -22,6 +24,29 @@ object PersistenceAPI {
 
     implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "BattleshipHTTPServer")
     implicit val executionContext: ExecutionContextExecutor = system.executionContext
+
+    implicit val vectorIntUnmarshaller: FromStringUnmarshaller[Vector[Int]] =
+      Unmarshaller.strict[String, Vector[Int]] { str =>
+        Try {
+          str.trim.stripPrefix("[").stripSuffix("]").split(",").toVector.map(_.trim.toInt)
+        }.getOrElse(Vector.empty[Int])
+      }
+
+
+    implicit val vectorVectorIntUnmarshaller: FromStringUnmarshaller[Vector[Vector[Int]]] =
+      Unmarshaller.strict[String, Vector[Vector[Int]]] { str =>
+        Try {
+          // Remove the outer brackets and trim spaces
+          val trimmedStr = str.trim.stripPrefix("[").stripSuffix("]")
+
+          // Split the string into individual inner vectors, accounting for optional spaces around brackets
+          trimmedStr.split("\\s*\\],\\s*\\[").toVector.map { innerStr =>
+            // Remove any remaining brackets, trim spaces, and split by comma
+            innerStr.trim.stripPrefix("[").stripSuffix("]").split("\\s*,\\s*").toVector.map(_.trim.toInt)
+          }
+        }.getOrElse(Vector.empty[Vector[Int]])
+      }
+
 
     val route = concat(
       path("persistence" / "load") {
@@ -85,39 +110,45 @@ object PersistenceAPI {
           }
         }
       },
-//      path("persistence" / "save") {
-//        post {
-//          formFields(
-//            "currentState".as[Int],
-//            "gameState".as[String],
-//            "gridSize1".as[Int],
-//            "gridSize2".as[Int],
-//            "name1".as[String],
-//            "name2".as[String],
-//            "shotsX1".as[Vector[Int]],
-//            "shotsY1".as[Vector[Int]],
-//            "shotsX2".as[Vector[Int]],
-//            "shotsY2".as[Vector[Int]],
-//            "shipsX1".as[Vector[Vector[Int]]],
-//            "shipsY1".as[Vector[Vector[Int]]],
-//            "shipsX2".as[Vector[Vector[Int]]],
-//            "shipsY2".as[Vector[Vector[Int]]]
-//          ) { () =>
-//            try {
-//              controller.addShot(x, y)
-//              complete(StatusCodes.OK, s"Added shot successfully")
-//            } catch {
-//              case e: Exception =>
-//                println(s"Exception during bet creation: ${e.getMessage}")
-//                complete(StatusCodes.InternalServerError, s"Failed due to internal server error: ${e.getMessage}")
-//            }
-//          }
-//        }
-//      },
+      path("persistence" / "save") {
+        post {
+          formFields(
+            "format".as[String],
+            "currentState".as[Int],
+            "gameState".as[String],
+            "gridSize1".as[Int],
+            "gridSize2".as[Int],
+            "name1".as[String],
+            "name2".as[String],
+            "shotsX1".as[Vector[Int]],
+            "shotsY1".as[Vector[Int]],
+            "shotsX2".as[Vector[Int]],
+            "shotsY2".as[Vector[Int]],
+            "shipsX1".as[Vector[Vector[Int]]],
+            "shipsY1".as[Vector[Vector[Int]]],
+            "shipsX2".as[Vector[Vector[Int]]],
+            "shipsY2".as[Vector[Vector[Int]]]
+          ) { (format, currentState, gameState, gridSize1, gridSize2, name1, name2, shotsX1, shotsY1, shotsX2, shotsY2, shipsX1, shipsY1, shipsX2, shipsY2) =>
+            try {
+              format match {
+                case "json" =>
+                  json.save(currentState, gameState, gridSize1, gridSize2, name1, name2, shotsX1, shotsY1, shotsX2, shotsY2, shipsX1, shipsY1, shipsX2, shipsY2)
+                case "xml" =>
+                  xml.save(currentState, gameState, gridSize1, gridSize2, name1, name2, shotsX1, shotsY1, shotsX2, shotsY2, shipsX1, shipsY1, shipsX2, shipsY2)
+              }
+              complete(StatusCodes.OK, s"Save game succeeded")
+            } catch {
+              case e: Exception =>
+                println(s"Exception during bet creation: ${e.getMessage}")
+                complete(StatusCodes.InternalServerError, s"Failed due to internal server error: ${e.getMessage}")
+            }
+          }
+        }
+      },
     )
 
-    val bindingFuture = Http().newServerAt("0.0.0.0", 8080).bind(route)
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    val bindingFuture = Http().newServerAt("0.0.0.0", 8081).bind(route)
+    println(s"Server online at http://localhost:8081/\nPress RETURN to stop...")
     StdIn.readLine() // Lässt den Server laufen, bis der Benutzer Return drückt
     bindingFuture
       .flatMap(_.unbind()) // Löst die Bindung vom Port
--- src/main/scala/de/htwg/se/battleship/Battleship.scala
+++ src/main/scala/de/htwg/se/battleship/Battleship.scala
@@ -3,7 +3,8 @@ package de.htwg.se.battleship
 import aview.TUI
 import com.google.inject.{Guice, Injector}
 import core.controller.ControllerInterface
-import core.controller.controllerImpl.Controller
+//import core.controller.controllerImpl.Controller
+import core.controller.controllerImpl.APIController
 import core.model.gridImpl.{Grid, Ship, ShipContainer, Shots}
 import gui.GUI
 
@@ -13,7 +14,7 @@ object Battleship {
 //  val injector: Injector = Guice.createInjector(new BattleshipModule)
 //  val controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
   val grid: Grid = Grid(10, Shots(Vector[Int](), Vector[Int]()), ShipContainer(Vector[Ship]()))
-  val controller = new Controller(grid)
+  val controller = new APIController(grid)
 
   val tui: TUI = TUI(controller)
   val gui = new GUI(controller)
