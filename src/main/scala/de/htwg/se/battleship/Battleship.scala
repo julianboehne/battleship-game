fc6e7aa810280ce97767529d5c13220d5d6ade9e
Fabian Meyer <meyer.fa@gmx.de>
2024-06-19 13:23:11 +0200

    updated Readme

--- src/main/scala/de/htwg/se/battleship/Battleship.scala
+++ src/main/scala/de/htwg/se/battleship/Battleship.scala
@@ -12,19 +12,25 @@ object Battleship {
   val grid: Grid = Grid(10, Shots(Vector[Int](), Vector[Int]()), ShipContainer(Vector[Ship]()))
   val controller = new Controller(grid)
 
-  // NODE: Default setup
+  //---------------------------------------------
+  // NOTE: Default setup (for Kafka setup comment out both lines:)
   val tui: TUI = TUI(controller)
   val gui = new GUI(controller)
+  //---------------------------------------------
 
-  // NODE: if you want to use Kafka instead
+  //---------------------------------------------
+  // NOTE: if you want to use Kafka instead comment in both lines:
   // val tui: TuiKafka = TuiKafka()
   // val consumer = new KafkaConsumer(controller)
+  //---------------------------------------------
 
 
   def main(args: Array[String]): Unit = {
     println("Welcome to Battleship-Game\n")
-    // NODE: if you want to use Kafka instead
+    //---------------------------------------------
+    // NOTE: if you want to use Kafka instead comment in this line:
     // consumer.start()
+    //---------------------------------------------
 
     while (true) {
       tui.processInputLine()
